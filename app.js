08dc12cc617be45ccc1adbf374e00508d5997e26
Vincent Dauliac <vincent.dauliac@gmail.com>
2016-04-21 17:18:43 +0200

    comment init db

--- app.js
+++ app.js
@@ -66,8 +66,9 @@ else mongoose.connect('mongodb://localhost/carduino', MongooseProdOptions);
 
 
 //----- MONGOOSE MODELS/SCHEMAS -----//
-var User = require('./models/user');
 
+var User = require('./models/user');
+/*
 var user = new User({
 	username: 'login',
 	password: 'pwd'
@@ -95,7 +96,7 @@ User.findOne({
 			console.log(valid ? "ValidAsync !!!" : "InvalidAsync"); //=>'ValidAsync'
 	});
 });
-
+*/
 
 function createToken(user, rememberme, callback) {
 	jwt.sign({
